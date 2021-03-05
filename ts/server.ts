a77eb680985fbc5f35f3431ad5e03dff978b74b4
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-03-05 16:52:43 +0100

    séance du 2021/03/05

--- ts/server.ts
+++ ts/server.ts
@@ -1,15 +1,14 @@
-import express  from "express";
+import express, { urlencoded }  from "express";
 import multer from 'multer';
 import commandLineArgs, { OptionDefinition } from 'command-line-args';
 import { check, validationResult, param } from 'express-validator';
 import path from "path";
+import fetch from 'node-fetch';
 const upload = multer({ dest: '../uploads/' }); // Pour les données form-data et multipart form data
 
 const app = express();
 const DIR = process.cwd();
 const PUB = path.join(DIR, '/public');
-console.log( DIR );
-console.log( PUB );
 
 app.use('/static', express.static( PUB ) );
 app.use( express.urlencoded({ extended: true }) );
@@ -21,6 +20,44 @@ const optionDefinitions: OptionDefinition[] = [
 const options = commandLineArgs(optionDefinitions)
 
 // Define HTTP API
+app.get('/TMDB/SEARCH', [check('query').isString()], async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        const query = req.body.query ?? req.params.query;
+        try {
+            const key = "9e0b7e444f3a564140d56af2346a8815";
+            const address = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${encodeURIComponent(query)}`;
+            const R = await fetch( address );
+            res.status(R.status).json( await R.json() );
+        } catch(err) {
+            res.status(500).json({ proxyError: err});
+        }
+
+    } else {
+        res.status(422).json({ errors: errors.array()});
+    }
+});
+
+app.get('/proxy', [check('URL').isString()], async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        const URL = req.body.URL;
+        console.log("On veut", URL);
+        
+        // res.json({code: 'WIP'});
+        try {
+            const R = await fetch(URL);
+            console.log("banco");
+            res.status(R.status).send( await R.text() );
+        } catch(err) {
+            res.status(500).json({ proxyError: err});
+        }
+
+    } else {
+        res.status(422).json({ errors: errors.array()});
+    }
+});
+
 app.get('/api/addition', upload.none(), [check('a').isNumeric(), check('b').isNumeric()], (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
